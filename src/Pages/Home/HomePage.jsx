76a6ec7f39ed664133c6f0289b2c83f53286b0b4
ahad30 <mohiminulislamahad@gmail.com>
2024-03-31 01:36:22 +0600

    ahad

---
+++ src/Pages/Home/HomePage.jsx
@@ -0,0 +1,12 @@
+const HomePage = () => {
+  return (
+    <div className="max-w-[1270px] mx-auto m-10">
+    <div className="bg-black h-10">
+
+    </div>
+      
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/Routes/routes.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../components/layouts/DashboardLayout/DashboardLayout";
+import MainLayout from "../components/layouts/MainLayout/MainLayout";
+import HomePage from "../Pages/Home/HomePage";
+import AboutPage from "../Pages/About/AboutPage";
+
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      // {
+      //   path: "/branch",
+      //   element: <BranchesPage />,
+      // },
+      {
+        path: "/login",
+        element: <div>Login</div>,
+      },
+      {
+        path: "/register",
+        element: <div>Register</div>,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/dashboard/admin",
+        element: <div>Admin</div>,
+      },
+    ],
+  },
+]);
